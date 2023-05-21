2177c6bd7e176ef558b210249d73158d849f1326
unknown <state.tudor2005@gmail.com>
2023-05-21 14:03:51 +0300

    Aceasta este ramura mea!

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main
+{
+    public static void main(String[] args)
+    {
+        String mesaj = "Acesta este primul meu program";
+        System.out.println(mesaj + " si sunt bucuros");
+        for (int i = 1; i <= 5; i++)
+        {
+            System.out.println("i = " + i);
+        }
+    }
+}
